6797ced123d9502dc310dc028c061c9712dd1c88
Trần Quang Huy <quanghuy7927@gmail.com>
2024-04-30 10:57:08 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(morgan('common'));
 app.use(cookieParser());
 app.use(express.json());
 
+app.use('/', express.static(path.join(__dirname + '/views')));
+
 app.get('/hello', (req, res) => {
     res.json({ message: 'Hello World!' });
-});
-
-app.get('/get-value', (req, res) => {
-    res.json({ message: process.env.TEST_VALUE });
 });
 
 app.listen(PORT, () => {
